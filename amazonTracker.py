894e4523dfef6a99b5e9fd8ed2ba1950728347f6
yoshi-non <himekawa.yoshino44@gmail.com>
2022-07-03 01:56:01 +0900

    add README

--- amazonTracker.py
+++ amazonTracker.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
 
 amazonURL = "https://www.amazon.co.jp/React%E3%83%8F%E3%83%B3%E3%82%BA%E3%82%AA%E3%83%B3%E3%83%A9%E3%83%BC%E3%83%8B%E3%83%B3%E3%82%B0-%E7%AC%AC2%E7%89%88-%E2%80%95Web%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E9%96%8B%E7%99%BA%E3%81%AE%E3%83%99%E3%82%B9%E3%83%88%E3%83%97%E3%83%A9%E3%82%AF%E3%83%86%E3%82%A3%E3%82%B9-Alex-Banks/dp/4873119383/ref=sr_1_1?crid=1BCQ7J8Y6KZGQ&keywords=amazon+react&qid=1656773731&sprefix=%2Caps%2C441&sr=8-1"
 
@@ -21,10 +24,11 @@ def amazonTrackingPrice():
 
 def sendLineNotify():
     print("lineに通知がいきました")
+    # lineNotifyToken = "os.environ['TOKEN_KEY']"
     lineNotifyToken = "your token"
     lineNotifyApi = "https://notify-api.line.me/api/notify"
     headers = {"Authorization": f"Bearer {lineNotifyToken}"}
-    data = {"message": "今がお買い時です! https://www.amazon.co.jp/React%E3%83%8F%E3%83%B3%E3%82%BA%E3%82%AA%E3%83%B3%E3%83%A9%E3%83%BC%E3%83%8B%E3%83%B3%E3%82%B0-%E7%AC%AC2%E7%89%88-%E2%80%95Web%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E9%96%8B%E7%99%BA%E3%81%AE%E3%83%99%E3%82%B9%E3%83%88%E3%83%97%E3%83%A9%E3%82%AF%E3%83%86%E3%82%A3%E3%82%B9-Alex-Banks/dp/4873119383/ref=sr_1_1?crid=1BCQ7J8Y6KZGQ&keywords=amazon+react&qid=1656773731&sprefix=%2Caps%2C441&sr=8-1"}
+    data = {"message": "今がお買い時です!" + amazonURL}
     requests.post(lineNotifyApi, headers=headers, data=data)
 
 
